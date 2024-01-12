cd05d3754e6e7d2dbb081e0dfa90e00033dbec08
Deepak jaat <deepakjhajhria4657@gmail.com>
2024-01-12 23:19:20 +0530

    sectionFirstSecondUpload

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,26 +1,81 @@
-import React from 'react'
-import { Cart, Logo, SearchIcon } from './Icons'
+import React, { useEffect, useState } from 'react'
+import { Cart, CrossIcon, Logo, SearchIcon } from './Icons'
 import { Link, useLocation } from 'react-router-dom'
+import { Sling as Hamburger } from 'hamburger-react'
+import CommonBtn from './CommonBtn'
 
 const NavBar = () => {
     const location = useLocation();
+    const [isOpen, setOpen] = useState(false)
+    const [search, setSearch] = useState(false)
+    const [input, setInput] = useState([]);
+    useEffect(() => {
+        if (search) {
+            document.body.classList.add('overflow-hidden');
+        } else {
+            document.body.classList.remove('overflow-hidden');
+        }
+    }, [search]);
+    const openinput = (item) => {
+        setInput([...input, item]);
+        setSearch(true);
+    };
+
+    const closeModal = () => {
+        setSearch(false);
+    };
+    if (isOpen == true) {
+        document.body.classList.add("overflow-hidden")
+    }
+    else {
+        document.body.classList.remove("overflow-hidden")
+    }
+    function RemoveOverflow() {
+        setOpen(false)
+        document.body.classList.remove("overflow-hidden")
+    }
     return (
         <nav className='py-[15px]'>
             <div className='container max-w-[1320px] flex justify-between items-center'>
-                <a className='flex items-center gap-[5px] sm:gap-2 uppercase font-Roboto font-bold text-white text-[22px] sm:text-2xl md:text-[30px] lg:text-[33px]' href="#"><Logo />FURNITURE</a>
-                {/* <div className='flex items-center gap-10'>
-                    <ul className='flex items-center gap-[55px]'>
-                        <li><Link to="/" className={`${location.pathname === "/" && " !font-bold relative after:absolute after:w-full after:duration-300 after:h-[3px] after:bg-[#BD7D41] after:-bottom-4 after:left-0"} text-white text-lg font-normal font-Roboto`}>HOME</Link></li>
-                        <li><Link to="/about" className={`${location.pathname === "/about" && " !font-bold relative after:absolute after:w-full after:duration-300 after:h-[3px] after:bg-[#BD7D41] after:-bottom-4 after:left-0"} text-white text-lg font-normal font-Roboto`}>ABOUT US</Link></li>
-                        <li><Link to="/shop" className={`${location.pathname === "/shop" && " !font-bold relative after:absolute after:w-full after:duration-300 after:h-[3px] after:bg-[#BD7D41] after:-bottom-4 after:left-0"} text-white text-lg font-normal font-Roboto`}>SHOP</Link></li>
-                        <li><Link to="/contactus" className={`${location.pathname === "/contactus" && " !font-bold relative after:absolute after:w-full after:duration-300 after:h-[3px] after:bg-[#BD7D41] after:-bottom-4 after:left-0"} text-white text-lg font-normal font-Roboto`}>CONTACT US</Link></li>
+                <a className='flex items-center gap-[5px] sm:gap-2 uppercase font-Roboto font-bold text-white text-x xs:text-[22px] sm:text-2xl md:text-[30px] lg:text-[33px]' href="#"><Logo />FURNITURE</a>
+                <div className='flex items-center gap-3 xs:gap-5 sm:gap-10'>
+                    <ul className={`${isOpen ? "right-0" : "-right-full"} flex items-center gap-[55px] mobileview`}>
+                        <li><Link to="/" onClick={RemoveOverflow} className={`${location.pathname === "/" && " !font-bold relative after:absolute after:w-full after:duration-300 after:h-[3px] after:bg-[#BD7D41] after:-bottom-4 after:left-0"} text-white text-lg font-normal font-Roboto`}>HOME</Link></li>
+                        <li><Link to="/about" onClick={RemoveOverflow} className={`${location.pathname === "/about" && " !font-bold relative after:absolute after:w-full after:duration-300 after:h-[3px] after:bg-[#BD7D41] after:-bottom-4 after:left-0"} text-white text-lg font-normal font-Roboto`}>ABOUT US</Link></li>
+                        <li><Link to="/shop" onClick={RemoveOverflow} className={`${location.pathname === "/shop" && " !font-bold relative after:absolute after:w-full after:duration-300 after:h-[3px] after:bg-[#BD7D41] after:-bottom-4 after:left-0"} text-white text-lg font-normal font-Roboto`}>SHOP</Link></li>
+                        <li><Link to="/contactus" onClick={RemoveOverflow} className={`${location.pathname === "/contactus" && " !font-bold relative after:absolute after:w-full after:duration-300 after:h-[3px] after:bg-[#BD7D41] after:-bottom-4 after:left-0"} text-white text-lg font-normal font-Roboto`}>CONTACT US</Link></li>
                     </ul>
-                    <div className='flex items-center gap-7'>
-                        <SearchIcon />
+                    <div className='flex items-center gap-5 xs:gap-7'>
+                        <div onClick={() => openinput('item')} className=' cursor-pointer'>
+                            <SearchIcon />
+                        </div>
                         <Cart />
                     </div>
-                </div> */}
+                    <div className='lg:hidden relative z-50 -mr-2'>
+                        <Hamburger toggled={isOpen} toggle={setOpen} size={30} rounded distance="md" direction='right' duration={0.5} color='#FFF' />
+                    </div>
+                </div>
             </div>
+            {isOpen && (
+                <div className='w-full bg-gray-200 flex h-full fixed opacity-40 top-0 left-0 z-30'>
+                </div>
+            )}
+            {search && (
+                <div className='w-full bg-gray-200 flex h-full fixed opacity-40 top-0 left-0 z-30'>
+                </div>
+            )}
+            {search && (
+                <div>
+                    <div className='bg-white max-w-[300px] xs:max-w-[380px] sm:max-w-[470px] md:max-w-[600px] min-h-[50px] sm:min-h-[80px] shadow-2xl rounded-full sm:rounded-[40px] fixed z-40 w-full flex flex-col justify-center items-center top-10 left-1/2 -translate-x-1/2'>
+                        <div className='flex w-full items-center justify-between'>
+                            <input className='px-10 text-black text-base sm:text-xl font-Poppins font-normal w-[90%] bg-transparent outline-none' placeholder='Search...' type="text" name="" id="" />
+                            <span onClick={closeModal} className="cursor-pointer pr-10">
+                                <CrossIcon />
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            )}
         </nav>
     )
 }
--- src/components/OurProducts.jsx
+++ src/components/OurProducts.jsx
@@ -6,41 +6,75 @@ import product4 from '../assets/images/singlesofa7.webp'
 import product5 from '../assets/images/singlesofa8.webp'
 import product6 from '../assets/images/singlesofa9.webp'
 import { Review } from './Icons'
+import { useEffect, useState } from 'react'
 
 const OurProducts = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [cart, setCart] = useState([]);
+    useEffect(() => {
+        if (isOpen) {
+            document.body.classList.add('overflow-hidden');
+        } else {
+            document.body.classList.remove('overflow-hidden');
+        }
+    }, [isOpen]);
+    const addToCart = (item) => {
+        setCart([...cart, item]);
+        setIsOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsOpen(false);
+    };
     const products = [
-        {img: product1, productName: "Single Sofa", productPara: "Lorem ipsum dolor", review: (<Review />), price: "₹ 1200.00", cartBtn: (<CommonBtn cstm="!py-[10px] !px-3" text="ADD TO CART" />)},
-        {img: product2, productName: "Single Sofa", productPara: "Lorem ipsum dolor", review: (<Review />), price: "₹ 1200.00", cartBtn: (<CommonBtn cstm="!py-[10px] !px-3" text="ADD TO CART" />)},
-        {img: product3, productName: "Single Sofa", productPara: "Lorem ipsum dolor", review: (<Review />), price: "₹ 1200.00", cartBtn: (<CommonBtn cstm="!py-[10px] !px-3" text="ADD TO CART" />)},
-        {img: product4, productName: "Single Sofa", productPara: "Lorem ipsum dolor", review: (<Review />), price: "₹ 1200.00", cartBtn: (<CommonBtn cstm="!py-[10px] !px-3" text="ADD TO CART" />)},
-        {img: product5, productName: "Single Sofa", productPara: "Lorem ipsum dolor", review: (<Review />), price: "₹ 1200.00", cartBtn: (<CommonBtn cstm="!py-[10px] !px-3" text="ADD TO CART" />)},
-        {img: product6, productName: "Single Sofa", productPara: "Lorem ipsum dolor", review: (<Review />), price: "₹ 1200.00", cartBtn: (<CommonBtn cstm="!py-[10px] !px-3" text="ADD TO CART" />)},
+        { img: product1, productName: "Single Sofa", productPara: "Lorem ipsum dolor", review: (<Review />), price: "₹ 1200.00", cartBtn: (<CommonBtn cstm="!py-[10px] !px-3" text="ADD TO CART" />) },
+        { img: product2, productName: "Single Sofa", productPara: "Lorem ipsum dolor", review: (<Review />), price: "₹ 1200.00", cartBtn: (<CommonBtn cstm="!py-[10px] !px-3" text="ADD TO CART" />) },
+        { img: product3, productName: "Single Sofa", productPara: "Lorem ipsum dolor", review: (<Review />), price: "₹ 1200.00", cartBtn: (<CommonBtn cstm="!py-[10px] !px-3" text="ADD TO CART" />) },
+        { img: product4, productName: "Single Sofa", productPara: "Lorem ipsum dolor", review: (<Review />), price: "₹ 1200.00", cartBtn: (<CommonBtn cstm="!py-[10px] !px-3" text="ADD TO CART" />) },
+        { img: product5, productName: "Single Sofa", productPara: "Lorem ipsum dolor", review: (<Review />), price: "₹ 1200.00", cartBtn: (<CommonBtn cstm="!py-[10px] !px-3" text="ADD TO CART" />) },
+        { img: product6, productName: "Single Sofa", productPara: "Lorem ipsum dolor", review: (<Review />), price: "₹ 1200.00", cartBtn: (<CommonBtn cstm="!py-[10px] !px-3" text="ADD TO CART" />) },
     ]
     return (
-        <div className='container max-w-[1320px] mt-[135px]'>
-            <p className='text-[#BD7D41] text-lg sm:text-xl font-Poppins font-normal headingLine'>Shopping Store</p>
-            <div className='flex w-full flex-col gap-4 sm:flex-row  sm:justify-between items-start sm:items-center mt-3'>
-                <h2 className='text-[#243040] text-2xl sm:text-3xl lg:text-[35px] font-Poppins font-bold max-w-[436px] mt-3 md:mt-4 lg:mt-5'>Our Products</h2>
-                <CommonBtn text="VIEW ALL PRODUCT" />
-            </div>
-            <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 w-full lg:gap-5 xl:gap-[27px] my-8 sm:my-10 md:my-16 lg:my-[90px]'>
-                {products.map((items, index) => (
-                    <div key={index} className='w-full bg-white duration-300 shadow-[0_0_0_0_rgba(0,0,0,0.12)] hover:shadow-[6px_13px_38px_0_rgba(0,0,0,0.12)] mb-4'>
-                        <div className='px-[23px] py-[18px]'>
-                            <img className='w-full' src={items.img} alt={items.productName} />
-                            <div className='flex items-center justify-between mt-5'>
-                                <p className='text-black font-Poppins font-medium text-lg sm:text-xl leading-normal'>{items.productName}</p>
-                                <span>{items.review}</span>
-                            </div>
-                            <p className='text-[#4D4D4D] text-base sm:text-lg font-Poppins font-normal mt-4 leading-normal'>{items.productPara}</p>
-                            <div className='flex w-full items-center justify-between mt-4 pb-[35px]'>
-                                <p className='text-black text-xl sm:text-2xl lg:text-[28px] font-Poppins font-semibold leading-normal'>{items.price}</p>
-                                {items.cartBtn}
+        <div className='relative'>
+            <div className='container max-w-[1320px] mt-[135px] relative'>
+                <p className='text-[#BD7D41] text-lg sm:text-xl font-Poppins font-normal headingLine'>Shopping Store</p>
+                <div className='flex w-full flex-col gap-4 sm:flex-row  sm:justify-between items-start sm:items-center mt-3'>
+                    <h2 className='text-[#243040] text-2xl sm:text-3xl lg:text-[35px] font-Poppins font-bold max-w-[436px] mt-3 md:mt-4 lg:mt-5'>Our Products</h2>
+                    <CommonBtn text="VIEW ALL PRODUCT" />
+                </div>
+                <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 w-full lg:gap-5 xl:gap-[27px] my-8 sm:my-10 md:my-16 lg:my-[90px]'>
+                    {products.map((items, index) => (
+                        <div key={index} className='w-full bg-white duration-300 shadow-[0_0_0_0_rgba(0,0,0,0.12)] hover:shadow-[6px_13px_38px_0_rgba(0,0,0,0.12)] mb-4'>
+                            <div className='px-[23px] py-[18px]'>
+                                <img className='w-full' src={items.img} alt={items.productName} />
+                                <div className='flex items-center justify-between mt-5'>
+                                    <p className='text-black font-Poppins font-medium text-lg sm:text-xl leading-normal'>{items.productName}</p>
+                                    <span>{items.review}</span>
+                                </div>
+                                <p className='text-[#4D4D4D] text-base sm:text-lg font-Poppins font-normal mt-4 leading-normal'>{items.productPara}</p>
+                                <div className='flex w-full items-center justify-between mt-4 pb-[35px]'>
+                                    <p className='text-black text-xl sm:text-2xl lg:text-[28px] font-Poppins font-semibold leading-normal'>{items.price}</p>
+                                    <div onClick={() => addToCart('item')}>{items.cartBtn}</div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                    {isOpen && (
+                        <div>
+                            <div className='bg-white max-w-[300px] xs:max-w-[380px] sm:max-w-[470px] md:max-w-[600px] min-h-[250px] sm:min-h-[300px] md:min-h-[500px] shadow-2xl rounded-3xl sm:rounded-[40px] fixed w-full flex flex-col justify-center items-center z-10 top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2'>
+                                <div>
+                                    <h2 className='text-[#243040] text-2xl sm:text-3xl lg:text-[35px] font-Poppins font-bold max-w-[436px] mt-3 md:mt-4 lg:mt-5 text-center'>Successfully added to cart!</h2>
+                                    <div className='flex justify-center mt-5' onClick={closeModal}><CommonBtn text="close" /></div>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </div>
             </div>
+            {isOpen && (
+                <div className='w-full bg-gray-200 flex h-full absolute opacity-30 top-0 left-0'>
+                </div>
+            )}
         </div>
     )
 }
